4bf465b07a7b1460ecf6b1a5272070f87d948ea6
Nathan Toups <n@rjrbt.io>
2024-12-15 22:46:19 -0700

    feat: day 10 part 1

--- day_10/part_1/main.jl
+++ day_10/part_1/main.jl
@@ -1,20 +1,42 @@
-const CharGrid = Array{Char,2}
+const IntGrid = Array{Int,2}
 
-function newgridfromfile(file::String)::CharGrid
+function newgridfromfile(file::String)::IntGrid
     file_content = read(file, String)
     lines = split(file_content, "\n")
     height = length(lines)
     width = length(lines[1])
-    grid = CharGrid(undef, height, width)
+    grid = IntGrid(undef, height, width)
     for i in 1:height
-        grid[i, :] = collect(lines[i])
+        grid[i, :] = parse.(Int, collect(lines[i]))
     end
     return grid
 end
 
-function possible_paths(grid::CharGrid, position::CartesianIndex{2})::Int
+function crawl!(grid::IntGrid, position::CartesianIndex{2}, step::Int, endpoints::Set{CartesianIndex{2}})
+    v = Nothing
+    try
+        v = grid[position]
+    catch
+        return
+    end
+    if v != step
+        return
+    end
+    if v == 9
+        push!(endpoints, position)
+        return position
+    end
+    crawl!(grid, CartesianIndex(position[1] - 1, position[2]), step + 1, endpoints)
+    crawl!(grid, CartesianIndex(position[1] + 1, position[2]), step + 1, endpoints)
+    crawl!(grid, CartesianIndex(position[1], position[2] - 1), step + 1, endpoints)
+    crawl!(grid, CartesianIndex(position[1], position[2] + 1), step + 1, endpoints)
+end
+
+function possible_paths(grid::IntGrid, position::CartesianIndex{2})::Int
     # TODO: TBD, here we need to implement the logic to find the possible paths
-    return 1
+    endpoints = Set{CartesianIndex{2}}()
+    crawl!(grid, position, 0, endpoints)
+    return length(endpoints)
 end
 
 
@@ -22,15 +44,13 @@ function main()
     filepath = joinpath(@__DIR__, "input.txt")
     grid = newgridfromfile(filepath)
 
-    trailheads = findall(x -> x == '0', grid)
+    trailheads = findall(x -> x == 0, grid)
 
     sum = 0
     for trailhead in trailheads
         sum += possible_paths(grid, trailhead)
     end
 
-    # println(grid)
-    println(trailheads)
     println(sum)
 end
 
